f2e97375b693854888b248a356c4d27674e86dfb
alastair.tibbs <alastiar.tibbs@co-operativebank.co.uk>
2022-05-17 22:54:20 +0100

    working ok

--- scraper.py
+++ scraper.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 with open('website-urls.json') as website_data:
     websites = json.load(website_data)
     for website in websites:
+        match_list = []
         url = website.get("url")
         method = website.get("method")
         print("\n" + url + "\n")
@@ -19,7 +20,10 @@ with open('website-urls.json') as website_data:
                     if re.search("\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])", href):
                         print(href)
                 if method == 2:
-                    print(href)
+                    if re.search("[^A-Za-z0-9](event|events)[^A-Za-z0-9][a-z|\-]*$", href):
+                        if href not in match_list:
+                            print(href)
+                            match_list.append(href)
         
         
         
