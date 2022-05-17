30bec1f63f37004a99b4ed43a7a4f7c5c8eedb2e
alastair.tibbs <alastiar.tibbs@co-operativebank.co.uk>
2022-05-17 22:26:30 +0100

    its is a beautiful soup

--- scraper.py
+++ scraper.py
@@ -12,11 +12,15 @@ with open('website-urls.json') as website_data:
         page_data = requests.get(url)
         page_str = page_data.text
         soup = BeautifulSoup(page_str, 'html.parser')
-        if method == 1:
-            for link in soup.find_all('a'):
-                href = link.get('href')
-                if not href is None:
+        for link in soup.find_all('a'):
+            href = link.get('href')
+            if not href is None:
+                if method == 1:
                     if re.search("\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])", href):
                         print(href)
+                if method == 2:
+                    print(href)
+        
+        
         
         
