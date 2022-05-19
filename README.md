5da7979f20d329ac89317e6135b5a280d2431e36
alastair.tibbs <alastiar.tibbs@co-operativebank.co.uk>
2022-05-19 12:29:11 +0100

    working roughly

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # gig-scraper
+
+This app scrapes gig venue websites for upcoming events and finds the artist playing at the event. It then takes this artists top song, using the spotify api, and puts it in a specified playlist.
+
+##Before Running
+
+1. Change "user_id" in global variables to your account
+2. Setup spotify developer account
+3. Setup SpotifyOAuth - https://spotipy.readthedocs.io/en/2.19.0/#authorization-code-flow
+4. Add venue websites to website-urls.json and decide best scraping method 
--- scraper.py
+++ scraper.py
@@ -3,14 +3,19 @@ import re
 import json
 from bs4 import BeautifulSoup
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
 import os
 
-client_id="49e1ae4924774066a717ec7f3cf229d8"
-client_secret=os.environ.get("CLIENT_SECRET")
-auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
-sp = spotipy.Spotify(auth_manager=auth_manager)
+scope = "playlist-modify-public"
 user_id = "11154880367"
+
+#Authorization Code Flow
+os.environ["SPOTIPY_CLIENT_ID"] = "49e1ae4924774066a717ec7f3cf229d8"
+os.environ["SPOTIPY_REDIRECT_URI"] = "http://localhost:8080"
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+playlists = sp.user_playlists(user_id)
+print(f"\nConfiguring playlist: {playlists['items'][0]['name']}")
+playlist_id = playlists['items'][0]['id']
 
 def get_website_tracks():
     match_list = []
@@ -26,7 +31,7 @@ def get_website_tracks():
         if number_added > 3:
             print(f"SUCCESS: HIT {number_added} TRACKS FOR VENUE")
             return 
-        if not href is None:
+        if href:
             if method == 1:
                 if method_1(href):
                     number_added = number_added + 1
@@ -51,7 +56,7 @@ def method_1(href):
 def get_artist_uri(name):
     results = sp.search(q='artist:' + name, type='artist')
     items = results['artists']['items']
-    if len(items) > 0:
+    if items:
         artist = items[0]
         add_top_song(artist)
         return 1
@@ -61,18 +66,16 @@ def add_top_song(artist):
     top_tracks = sp.artist_top_tracks(artist['uri'])
     top_track = top_tracks['tracks'][0]['uri']
     print(f"{artist['name']} - {top_track}")
+    top_track_uri = [f"{top_track}"]
+    sp.playlist_add_items(playlist_id, top_track_uri)
     return
 
 if __name__=="__main__":
-    playlists = sp.user_playlists(user_id)
-    print(f"\nConfiguring playlist: {playlists['items'][0]['name']}")
-    playlist_id = playlists['items'][0]['id']
     playlist_items = sp.playlist_items(playlist_id)
     item_id_list = []
-    for item in playlist_items['items']:
+    for item in playlist_items['items'][1:]:
         item_id_list.append(item['track']['id'])
-    print(item_id_list)
-    sp.playlist_remove_all_occurrences_of_items(playlist_id, playlist_items)
+    sp.playlist_remove_all_occurrences_of_items(playlist_id, item_id_list)
     with open('website-urls.json') as website_data:
         websites = json.load(website_data)
         for website in websites: 
